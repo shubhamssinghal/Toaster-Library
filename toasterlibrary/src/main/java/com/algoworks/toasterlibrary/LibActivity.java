dd8ce8db5ad265901ccc9dc1d4e707626dbd1dc5
Shubham <shubhams.singhal@mobicules.com>
2019-03-06 16:40:21 +0530

    Three digits 1.0.6

--- toasterlibrary/src/main/java/com/algoworks/toasterlibrary/LibActivity.java
+++ toasterlibrary/src/main/java/com/algoworks/toasterlibrary/LibActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 public class LibActivity extends AppCompatActivity {
 
 
-    EditText edt1,edt2;
+    EditText edt1,edt2,edt3;
     Button btn1;
     int a,b,c;
 
@@ -21,6 +21,7 @@ public class LibActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lib);
         edt1=findViewById(R.id.edt1);
         edt2=findViewById(R.id.edt2);
+        edt3=findViewById(R.id.edt3);
         btn1=findViewById(R.id.btn1);
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -35,32 +36,35 @@ public class LibActivity extends AppCompatActivity {
 
     public void buttonClick(Context context, Button btn1, final EditText edt1, final EditText edt2)
     {
-        { if (edt1.getText().toString().equalsIgnoreCase(""))
+        a=Integer.parseInt(edt1.getText().toString());
+        b=Integer.parseInt(edt2.getText().toString());
+            if (edt1.getText().toString().equalsIgnoreCase(""))
             {
                 Toast.makeText(context, "Enter first number", Toast.LENGTH_SHORT).show();
+                edt1.requestFocus();
             }
             else
             {
-                a=Integer.parseInt(edt1.getText().toString());
-
-                if (edt1.getText().toString().equalsIgnoreCase(""))
+                if (edt2.getText().toString().equalsIgnoreCase(""))
                 {
                     Toast.makeText(context, "Enter second number", Toast.LENGTH_SHORT).show();
+                    edt2.requestFocus();
                 }
                 else
                 {
-                    b=Integer.parseInt(edt2.getText().toString());
+                    if (edt3.getText().toString().equalsIgnoreCase(""))
+                    {
+                        Toast.makeText(context, "Enter third number", Toast.LENGTH_SHORT).show();
+                        edt3.requestFocus();
+                    }
+                    else
+                    {
+                        c=a+b;
+                        Toast.makeText(context, "Sum : "+c, Toast.LENGTH_SHORT).show();
+                    }
 
-                    c=a+b;
-                    Toast.makeText(context, "Sum : "+c, Toast.LENGTH_SHORT).show();
                 }
-
             }
-
-
-
-        }
-
     }
 
 }
